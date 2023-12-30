b13d022f511e103267afacb756f04583db2d24a0
alexellis2010 <155187193+alexellis2010@users.noreply.github.com>
2023-12-30 13:55:44 -0600

    Adding questions and answers to project (#5)
    * Adding questions and answers to project
    
    * Update index.js
    
    Co-authored-by: David Ellis <isv.damocles@gmail.com>
    
    ---------
    
    Co-authored-by: David Ellis <isv.damocles@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Goodbye, World!")
+console.log("goodbye world")
+
+console.log([1, 2, 3]);
+
+for (const element of [1, 2, 3]) {
+    console.log(element * element);
+}
+
+const obj = {"q": "How do you even?", "a": "I odd", "z": [1, 2, 3]};
+
+console.log(obj);
+
+for (const key of Object.keys(obj)) {
+    console.log(key);
+    console.log(obj[key]);
+    for (const char of obj[key]) {
+        console.log(char);
+        if (char instanceof String) console.log(char.charCodeAt());
+    }
+}
+
+console.log([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]);
