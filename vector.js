c3e81481ebe6a333cf2425b432b256ba4e7fb5c3
Alexander Ellis <alexellis2010@gmail.com>
2023-12-30 14:22:51 -0600

    we are putting the vectors into the json object

--- vector.js
+++ vector.js
@@ -1,16 +1,29 @@
+const qas = require('./qa.json');
 const OpenAI = require("openai");
 
 const openai = new OpenAI();
 
 async function main() {
-  const embedding = await openai.embeddings.create({
-    model: "text-embedding-ada-002",
-    input: "The quick brown fox jumped over the lazy dog",
-    encoding_format: "float",
-  });
-
-  console.log(embedding);
-  console.log(JSON.stringify(embedding.data[0].embedding));
+    let out = [];
+    for (const qa of qas) {
+        let obj = {};
+        const qe = await openai.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: qa.q,
+            encoding_format: "float",
+        });
+        const ae = await openai.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: qa.a,
+            encoding_format: "float",
+        })
+        obj.q = qa.q;
+        obj.a = qa.a;
+        obj.qe = qe.data[0].embedding;
+        obj.ae = ae.data[0].embedding;
+        out.push(obj);
+    }
+    console.log(JSON.stringify(out));
 }
 
 main();
