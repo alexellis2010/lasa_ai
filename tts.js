c85a19ba5874eaabd859a9c7c6785d60b560dc0e
alexellis2010 <155187193+alexellis2010@users.noreply.github.com>
2023-12-30 18:02:43 -0600

    example TTS output (#8)

---
+++ tts.js
@@ -0,0 +1,28 @@
+const sdl = require('@kmamal/sdl');
+const { Lame, } = require('node-lame');
+const OpenAI = require("openai");  
+
+const openai = new OpenAI();
+
+async function main() {
+    const mp3 = await openai.audio.speech.create({
+        model: "tts-1",
+        voice: "onyx",
+        input: "Today is a wonderful day to build something people love!",
+    });
+    const mp3buffer = Buffer.from(await mp3.arrayBuffer());
+    const decoder = new Lame({ output: 'buffer', bitwidth: 32, 'to-mono': true, 'little-endian': true}).setBuffer(mp3buffer)
+    await decoder.decode();
+    const buffer = decoder.getBuffer();
+    const playbackInstance = sdl.audio.openDevice({ type: 'playback' }, { channels: 1, format: 's32lsb', frequency: 12000, });
+    playbackInstance.enqueue(buffer);
+    playbackInstance.play();
+    await new Promise((r) => i = setInterval(() => {
+        if (playbackInstance.queued === 0) {
+            playbackInstance.close();
+            clearInterval(i);
+            r();
+        }
+    }, 200));
+}
+main();
