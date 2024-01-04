d49dea6b2b77c6ce92af9bd4db6bf544a5a86173
Alexander Ellis <alexellis2010@gmail.com>
2024-01-04 16:32:17 -0600

    Full chat loop with vector db connected

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const { LocalIndex } = require('vectra');
 const qavector = require('./qavector.json');
+const OpenAI = require("openai");
+
+const openai = new OpenAI();
 
 const index = new LocalIndex(path.join(__dirname, 'index'));
 
-async function main() {
+module.exports.query = async function query(question) {
     if (!await index.isIndexCreated()) {
         await index.createIndex();
         for (const qa of qavector) {
@@ -18,6 +21,10 @@ async function main() {
             });
         }
     }
-    console.dir(await index.queryItems(qavector[0].qe, 3), { depth: 4 });
-}
-main();
+    const qe = await openai.embeddings.create({
+        model: "text-embedding-ada-002",
+        input: question,
+        encoding_format: "float",
+    });
+    return (await index.queryItems(qe.data[0].embedding, 3)).map(i => i.item.metadata);
+};
