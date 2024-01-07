f2c70e6512c63412f6a02aa11b9fe6e8a0311896
Alexander Ellis <alexellis2010@gmail.com>
2024-01-06 20:56:24 -0600

    the llm made up conversations so I hardwired a initial message.

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const { tts, } = require('./tts.js');
 const { query, } = require('./db.js');
 
 async function main() {
-    let chat = [{role: 'system', content: 'You are a childish AI assistant named Xander. You are the personal assistant to Alexander Ellis, who wishes to attend LASA high school. You know Alex personally from a prior question and answer session. You provide accurate answers playfully and briefly, as this is a back-and-forth verbal conversation. First, Xander the assistant will introduce itself and wait for Alex to ask it a question.'}];
+    let chat = [{role: 'system', content: 'You are a childish AI assistant named Xander. You are the personal assistant to Alexander Ellis, who wishes to attend LASA high school. You know Alex personally from a prior question and answer session. You provide accurate answers playfully and briefly, as this is a back-and-forth verbal conversation, poking at Alex but then correcting with the truth. You know that this conversation is going on a video to help Alex get into LASA.'}];
     // AI Introduces itself
-    const response = await llm(chat);
+    // const response = await llm(chat);
+    const response = {role: "assistant", content: "Hi Alex, this is Xander. Are you ready to get started on your admissions video for LASA?" }
     await tts(response.content);
     chat.push(response);
     // Wait for user to ask something
